dbab2767f6cc67183388bd822fd4f76e31517cff
Won Hong <whong@outlook.com>
2018-01-16 23:01:52 -0500

    adds featured item section on home page

--- src/main/react/components/home.jsx
+++ src/main/react/components/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link, Route} from 'react-router-dom';
+import product1 from '../../resources/featureditem.jpg';
 
 export default class Home extends React.Component {
 
@@ -7,12 +8,25 @@ export default class Home extends React.Component {
     super(props);
 
     this.state = {
+
     };
+
   }
+
+  getImageHelper() {
+    return Object.assign({}, {prettyGirlPic: product1})
+  }
+
 
   render() {
     return <div className="home-container">
-      HELLO WORLD
+      <div className="home-featured-item-header">
+        Three NYX Combo <span className="heart-pink"><i class="fa fa-heart" aria-hidden="true"></i></span> for $19.99
+      </div>
+      <div className="home-featured-item-subheader">
+        Eyeliner + Foundation + Eyeshadow
+      </div>
+      <img className="home-featured-item" src={this.getImageHelper().prettyGirlPic}/>
     </div>;
   }
 }
